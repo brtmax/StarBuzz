d808558919c7eebbb79f7b1d45f69bc7b24285d4
brtmax <bretschneider.max@web.de>
2021-04-26 09:15:14 +0200

    Added the possibility for different drink sizes in abstract component

--- src/max/headfirst/edu/Beverage.java
+++ src/max/headfirst/edu/Beverage.java
@@ -4,11 +4,22 @@ package max.headfirst.edu;
  * This is our abstract component
  */
 public abstract class Beverage {
+    public enum Size { TALL, GRANDE, VENTI };
+
+    Size size = Size.TALL;
+
     String description = "Unknown Beverage";
 
     public String getDescription() {
         return description;
     }
 
+    public void setSize(Size size) {
+        this.size = size;
+    }
+
+    public Size getSize() {
+        return this.size;
+    }
     public abstract double cost();
 }
