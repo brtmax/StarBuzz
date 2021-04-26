9d7c71b42793ba2e97dcae54ffc840afc9651ccd
brtmax <bretschneider.max@web.de>
2021-04-26 08:47:03 +0200

    Added comments

--- src/max/headfirst/edu/CondimentDecorator.java
+++ src/max/headfirst/edu/CondimentDecorator.java
@@ -1,6 +1,14 @@
 package max.headfirst.edu;
 
+/**
+ * This is a project to demonstrate the Decorator Design Pattern from the
+ * HeadFirst Design patterns book
+ */
 public abstract class CondimentDecorator extends Beverage {
 
+    /**
+     * This is our abstract decorator
+     * @return
+     */
     public abstract String getDescription();
 }
